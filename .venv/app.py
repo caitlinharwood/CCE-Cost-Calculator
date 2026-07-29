fc4cd6198995c118211d30ac4f9c1e18366585fb
unknown <ceharwood03@gmail.com>
2026-07-29 15:03:42 -0500

    update

--- .venv/app.py
+++ .venv/app.py
@@ -292,6 +292,7 @@ if uploaded_file is not None:
         panel_eff = 1 - (degrad * (i - 1))
         disc_factor = 1 / ((1 + disc_rate) ** (i + 1))
         egenf = eff_rate_blend * (total_baseline_kwh * utility_escalation_factor * panel_eff)
+        st.write(total_baseline_kwh)
         st.write(egenf)
         #eff_utility_rate[0.141]
 
